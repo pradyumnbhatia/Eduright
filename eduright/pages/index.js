2e1db6a9448f513815dca434611659bb52635c04
pradyumnbhatia <shivam14ddn@gmail.com>
2022-08-07 03:40:43 +0530

    initial commit

---
+++ eduright/pages/index.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import CourseCard from "../components/cards/CourseCard";
+
+const Index = ({courses}) => {
+  /*const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const { data } = await axios.get("/api/courses");
+      setCourses(data);
+    };
+    fetchCourses();
+  }, []);
+*/
+  return (
+    <>
+      <h1 className="jumbotron p-3 text-center bg-primary square">
+        Online Education Marketplace
+      </h1>
+      <div className="container-fluid">
+        <div className="row">
+          {courses.map((course) => (
+            <div key={course._id} className="col-md-4">
+              <CourseCard course={course} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export async function getServerSideProps() {
+  const { data } = await axios.get(
+    `http://localhost:8000/api/courses`
+  );
+  return {
+    props: {
+      courses: data,
+    },
+  };
+}
+
+export default Index;
